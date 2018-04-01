69b0d0b25eb04233a9ce23d89a7358228dc829a0
thalerian <37544130+thalerian@users.noreply.github.com>
2018-04-01 14:02:25 +0300

    Update Problem_1.R

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,30 @@
 
+#a
+x <- c(4, 1, 1, 4)
+
+#b
+y<- c(1, 4)
+
+#c
+x-y
+#Answer to x-y = -217
+y-x
+#Answer to y-x = 217
+
+#d
+s <- c(x, y)
+
+
+#e
+rep(s, 10)
+length(rep(s, 10)) #=20
+
+#f
+rep(s, each=3)
+
+#g
+seq(7, 21)
+7:21
+
+#h
+length(7:21)
